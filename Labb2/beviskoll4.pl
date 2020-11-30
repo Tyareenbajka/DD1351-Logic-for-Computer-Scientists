8762a3dca9876914793e814809956238107edbf1
Tyareenbajka <58476139+Tyareenbajka@users.noreply.github.com>
2020-11-30 10:48:26 +0100

    finished

--- Labb2/beviskoll4.pl
+++ Labb2/beviskoll4.pl
@@ -1,39 +1,9 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%			Adeel Hussain & Philip Tonaczew 		    %
-% 				Labb 2 - 2020-11-17						%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Adeel Hussain & Philip Tonaczew 		    
+Labb 2 - 2020-11-17						
 
-% ['C:/Users/tonac/Desktop/Prolog/Labb2/beviskoll2.pl'].
-% ['C:/Users/tonac/Documents/GitHub/DD1351/Labb2/beviskoll4.pl'].
-% ['C:/Users/adeel/OneDrive/KTH/Årskurs 2/HT-20/Logik För Dataloger/DD1351/Labb 2/beviskoll2.pl'].
-%
-% cd C:/Users/tonac/Documents/GitHub/DD1351/Labb2/
-% swipl
-% ['test.pl'].
-% test
-
-% premise 			Klart
-% assumption		Klart
-% copy(x) 			Klart
-% andint(x,y) 		Klart
-% andel1(x) 		Klart
-% andel2(x) 		Klart
-% orint1(x) 		Klart
-% orint2(x) 		Klart
-% orel(x,y,u,v,w)	Klart 
-% impint(x,y) 		Klart
-% impel(x,y)		Klart
-% negint(x,y) 		Klart
-% negel(x,y) 		Klart
-% contel(x) 		Klart
-% negnegint(x) 		Klart
-% negnegel(x) 		Klart
-% mt(x,y) 			Klart (Ej Fullständig)
-% pbc(x,y) 			Klart
-% lem 				Klart
-%
-% valid16.txt failed. The proof is valid but your program rejected it!
-
+% ['beviskoll.pl'].
+% ['run_all_tests.pl'].
+% run_all_tests('beviskoll.pl').
 
 
 verify(InputFileName) :- see(InputFileName),
@@ -43,20 +13,16 @@ verify(InputFileName) :- see(InputFileName),
 
 valid_proof(Prems, Goal, Proof):- 
 	checkGoal(Goal, Proof),
-	checkProof(Prems, Proof, []), !,
-	write('Predikatet uppfyllt!').
+	checkProof(Prems, Proof, []), !.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%			Kontrollera Goal		    %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Kontrollera Goal		    
 
 checkGoal(Goal, Proof):- 
 	last(Proof, LastRow),
 	nth1(2, LastRow, Goal).
 	
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%			Kontrollera bevis rad för rad		    %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Kontrollera bevis rad för rad		    
 
 checkProof(_, [], _).
 checkProof(Prems, [H|T], CheckedList):- 
@@ -64,82 +30,57 @@ checkProof(Prems, [H|T], CheckedList):-
 	addToList(H, CheckedList, NewList),
 	checkProof(Prems, T, NewList).
 	
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%			Kontroll av regler		    %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Kollar om det är en premiss
 check_rule(Prems, [_, Atom, premise], _):-
-	member(Atom, Prems),
-	write('Premiss regeln!'),
-	write('\n').	
+	member(Atom, Prems).	
 
 % Kollar regel andint(X,Y)
 check_rule(_, [_, and(Atom1,Atom2), andint(X,Y)], CheckedList):-
 	member([X, Atom1, _], CheckedList),
-	member([Y, Atom2, _], CheckedList),
-	write('Andint regeln!'),
-	write('\n').
+	member([Y, Atom2, _], CheckedList).
 
 % Kollar regel orint1(X)
 check_rule(_,[_,or(Atom,_), orint1(Z)], CheckedList):-
-	member([Z,Atom,_], CheckedList),
-	write('Orint1 regeln!'),
-	write('\n').
+	member([Z,Atom,_], CheckedList).
 
 % Kollar regel orint2(X)
 check_rule(_,[_,or(_,Atom), orint2(Z)], CheckedList) :-
-	member([Z,Atom,_], CheckedList),
-	write('Orint2 regeln!'),
-	write('\n').
+	member([Z,Atom,_], CheckedList).
 
 % Kollar regel andel1         
 check_rule(_, [_, Atom, andel1(X)],CheckedList):-
-	member([X, and(Atom,_), _], CheckedList),
-	write('Andel1 regeln!'),
-	write('\n').
+	member([X, and(Atom,_), _], CheckedList).
 
 % Kollar regel andel2 
 check_rule(_, [_, Atom, andel2(X)],CheckedList):-
-	member([X, and(_,Atom), _],CheckedList),
-	write('Andel2 regeln!'),
-	write('\n').
+	member([X, and(_,Atom), _],CheckedList).
 
 % Kollar regel impel(x,y)
 check_rule(_, [_, Atom, impel(X,Y)],CheckedList):-
     member([X, Atom1,_],CheckedList),
-	member([Y, imp(Atom1,Atom),_],CheckedList),
-	write('Impel regeln!'),
-	write('\n').
+	member([Y, imp(Atom1,Atom),_],CheckedList).
 
 % Kollar regel lem
-check_rule(_, [_,or(Atom, neg(Atom)), lem],_):-
-	write('Orint2 regeln!'),
-	write('\n').
+check_rule(_, [_,or(Atom, neg(Atom)), lem],_).
+
 
 % Kollar regel copy(x)
 check_rule(_,[_,Atom, copy(X)],CheckedList):-
-	member([X,Atom,_],CheckedList),
-	write('Copy regeln!'),
-	write('\n').
+	member([X,Atom,_],CheckedList).
 
 %Kollar regel negel(x)
 check_rule(_,[_,cont, negel(X,Y)], CheckedList):-
 	member([X, Atom,_], CheckedList),
-	member([Y, neg(Atom),_], CheckedList),
-	write('Negel regeln!'),
-	write('\n').
+	member([Y, neg(Atom),_], CheckedList).
 
-% Kollar regel mt(x,y) (ska lägga in resternade tre scenario till)
+% Kollar regel mt(x,y) 
 check_rule(_,[_, neg(Atom), mt(X,Y)], CheckedList):-
 	member([X,imp(Atom,neg(Atom2)),_], CheckedList),
 	member([Y,neg(neg(Atom2)),_], CheckedList);
 
 	member([X,imp(Atom,Atom2),_], CheckedList),
-	member([Y,neg(Atom2),_], CheckedList),
-
-	write('MT regeln!'),
-	write('\n').
+	member([Y,neg(Atom2),_], CheckedList).
 
 % Kollar regel mt(x,y) fast dubbel negation framför
 check_rule(_,[_,neg(neg(Atom)), mt(X,Y)], CheckedList):-
@@ -147,64 +88,46 @@ check_rule(_,[_,neg(neg(Atom)), mt(X,Y)], CheckedList):-
 	member([Y,neg(Atom2),_], CheckedList);
 
 	member([X,imp(neg(Atom,neg(Atom2))),_], CheckedList),
-	member([Y,neg(neg(Atom2)),_], CheckedList),
-
-	write('MT regeln!'),
-	write('\n').
+	member([Y,neg(neg(Atom2)),_], CheckedList).
 
 % Kollar regel negnegint(x)
 check_rule(_,[_, neg(neg(Atom)), negnegint(X)], CheckedList):-
-	member([X, Atom,_], CheckedList),
-	write('Negnegint regeln!'),
-	write('\n').
+	member([X, Atom,_], CheckedList).
 
 % Kollar regel negnegel(x)
 check_rule(_,[_,Atom, negnegel(X)], CheckedList):-
-	member([X, neg(neg(Atom)),_], CheckedList),
-	write('Negnegel regeln!'),
-	write('\n').
+	member([X, neg(neg(Atom)),_], CheckedList).
 
 % Kollar regel contel(x)
 check_rule(_, [_, _, contel(X)], CheckedList):-
-	member([X, cont, _], CheckedList),
-	write('Contel regeln!'),
-	write('\n').
+	member([X, cont, _], CheckedList).
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%			Boxhantering & Regler		    %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Boxhantering & Regler
+
 
 %Kollar boxen och kallar checkProof som sedan rekursivt itererar igenom boxen
 check_rule(Prems, [[X, Atom, assumption]|T], CheckedList):-
 	addToList([X, Atom, assumption], CheckedList, NewList),
-	write('Assumption box regeln!'),
-	write('\n'),
 	checkProof(Prems,T,NewList).
 	
 % Kollar regeln negint
 check_rule(_, [_, neg(Atom), negint(X,Y)], CheckedList):-
 	member(BoxList, CheckedList),
 	member([X, Atom, assumption], BoxList),
-	member([Y, cont, _], BoxList),
-	write('Negint regeln!'),
-	write('\n').
+	member([Y, cont, _], BoxList).
 
 % Kollar regel impint
 check_rule(_, [_, imp(Atom1,Atom2), impint(X,Y)], CheckedList):-
 	member(BoxList, CheckedList),
 	member([X, Atom1, assumption], BoxList),
-	member([Y, Atom2, _], BoxList),
-	write('Impint regeln!'),
-	write('\n').
+	member([Y, Atom2, _], BoxList).
 
 % Kollar regel pbc
 check_rule(_, [_, Atom, pbc(X,Y)], CheckedList):-
 	member(BoxList, CheckedList),
 	member([X, neg(Atom), assumption], BoxList),
-	member([Y, cont, _], BoxList),
-	write('PBC regeln!'),
-	write('\n').
+	member([Y, cont, _], BoxList).
 
 % Kollar regel OR-eliminering orel(x,y,u,v,w)
 check_rule(_, [_, Atom, orel(S1,S2,S3,S4,S5)], CheckedList):-
@@ -214,13 +137,8 @@ check_rule(_, [_, Atom, orel(S1,S2,S3,S4,S5)], CheckedList):-
 	member([S2, Atom1, assumption], BoxList1),
 	member([S3,Atom, _], BoxList1),
 	member([S4,Atom2, assumption], BoxList2),
-	member([S5,Atom, _], BoxList2),
-	write('orel regeln!'),
-	write('\n').
+	member([S5,Atom, _], BoxList2).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%			Listhantering		     %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Lägger till ny lista
 addToList(H, CheckedList, NewList):-
